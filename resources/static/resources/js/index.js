2c72dad17e63d86fe7502ce0705add2226eee884
yong1227 <rnjs1227@naver.com>
2019-06-11 21:30:17 +0900

    from class to id

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -2,12 +2,12 @@ alert('자바스크리브 연결 성공!!');
 
 var wrapper = document.querySelector('#wrapper');
 wrapper.innerHTML = '<h1>SPA 시작</h1>'
-+'<div class=target>삭제할 내용</div>'
++'<div id=target>삭제할 내용</div>'
 +'<button id="btn">지워버리자</button>'
 +'<button id="btn2">추가하자</button>';
 
-var btn = document.querySelector('#btn');
 //btn은 객체가 된다. 셀렉할 수 있다. id나 class로 셀렉할 수 있으면 컴포넌트가 된다.
+var btn = document.querySelector('#btn');
 var target = document.querySelector("#target");
 
 btn.addEventListener('click',function(){
@@ -22,5 +22,3 @@ btn2.addEventListener('click',function(){
     var temp = document.createTextNode('새로 추가됨');
     target.appendChild(temp);
 });
-
-
