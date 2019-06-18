f82402cd7d1d457bbb58f7c4162d0e4e2d0a8727
yong1227 <rnjs1227@naver.com>
2019-06-18 15:01:41 +0900

    15:00 object literal, CustomerController before

--- java/com/bitcamp/web/controller/CustomerController.java
+++ java/com/bitcamp/web/controller/CustomerController.java
@@ -20,7 +20,7 @@ public class CustomerController {
     @Autowired 
     CustomerService customerService;
     @Autowired
-    CustomerDTO customerDTO;
+    CustomerDTO customer;
 
     @RequestMapping("/count")
     public String count(){
@@ -30,69 +30,11 @@ public class CustomerController {
         return count+"";
     }
 
-
-    @GetMapping("/customers/{customerId}")
+    @GetMapping("/customers/{customerId}/{password}")
     public CustomerDTO login(@PathVariable("customerId")String id,
-                                        @RequestAttribute CustomerDTO param) {
-        System.out.println("컨트롤러 넘어온 pw : "+param.getPassword());
-        customerDTO.setCustomerId(id);
-        customerDTO.setPassword(param.getPassword());
-        return customerService.login(customerDTO);
+                        @PathVariable("password")String pass){
+        customer.setCustomerId(id);
+        customer.setPassword(pass);
+        return customerService.login(customer); 
     }
-
-
-
-
-    // @RequestMapping("/login/{customerId}/{password}")    
-    // public  CustomerDTO login(@PathVariable("customerId")String id,
-    //                             @PathVariable("password")String pass){
-    //     System.out.println("AJAX로 넘어온 ID : "+id);
-    //     System.out.println("AJAX로 넘어온 ID : "+id);
-
-    //     //null일 수가 없다.
-    //     customerDTO.setCustomerId(id);
-    //     customerDTO.setPassword(pass);
-
-        // return (!customerService
-        //             .login(customerDTO)
-        //             .getCustomerName()
-        //             .equals("")
-        //                 ?"SUCCESS" : "FAIL");
-        
-        // return (customerService.login(customerDTO)!=null) ? "SUCCESS" : "FAIL";
-        // return customerService.login(customerDTO);
-
-        // 서비스까지 값을 보내야 한다. 
-        // Component있는 클래스에 있는 것까지 보내야 값이 안 없어진다.
-        // 바로 mapper까지 보내면 캡슐화의 의미가 없다.
-        // 그래서 서비스까지 보내기 위해 service 클래스를 호출한다.
-        // 인스턴스만 살아남아서 값을 전달할 수 있다.
-        // 값을 받아줘야 한다. 변수로 하나 받아준다.
-
-        // CustomerDTO t = customerService.login(customer);
-        // System.out.println("DB에서 넘어온 이름 : " + t.getCustomerName());
-
-        //DB에 값이 있건 없건 null이 아니라 DTO에는 값이 있다. 그 중에 아이디와 패스워드 값이 있는 거다. 위에 set을 해줘서.
-        //그래서 로그인(DB안에 데이터를 확인하는 거니까)하려면 다른 데이터를 DTO에 꺼내서 null인 지 확인해야 한다.
-        //name(이름)이 null이 아니면 로그인 성공, null이면 로그인 실패
-        //name(getCustomerName)를 변수에 담는다
-
-        // String name = t.getCustomerName();
-        //결과값 담을 문자열
-
-        // String loginResult = " ";
-
-        //있으면
-        // if(name.equals(" ")){
-        //     //문자열을 변수에 담는다.
-        //     loginResult= "SUCCESS";
-        //     System.out.println(loginResult);
-        // //없으면
-        // }else{
-        //     loginResult = "FAIL";
-        //     System.out.println(loginResult);
-        // }
-        // return loginResult;
-        // return (!name.equals(" ")) ?  "SUCCESS" : "FAIL";
-    // }
 }
--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,145 +1,86 @@
-//IIFE 실습
-//()안에 있는 것은 즉시 실행해라.
-// = 할당 연산자 썼다. init 도 기능이니까 app은 할당연산자 사용해서 속성으로 기능을 받았다.
-var app = (() => {
-    let $wrapper;
-    let init = () => {
-        $wrapper = document.querySelector('#wrapper');
-        //init 과 login_form은 형제관계라 호출 가능
-        login();
-    }
-
-    //login form만들기
-    let login = () => {
+// use
+var app =  {
+    $wrapper :  $wrapper = document.querySelector('#wrapper'),
+    init : ()=>{
         $wrapper.innerHTML = app.login_form();
-
-        // 회원가입 버튼을 선택할 수 있게 객체 만들기
-        let join_btn = document.querySelector('#join_btn')
-
-        // 옵저버 버튼/ 회원가입 버튼에 이벤트 걸기
-        join_btn.addEventListener('click',() => {
-            //회원가입 버튼 누르면 회원가입 폼 함수를 실행해줘
-            join_form();
+        let join_btn = document.querySelector('#join-btn');
+        join_btn.addEventListener('click',()=>{
+            $wrapper.innerHTML = app.join_form();
+            document.getElementById('confirm-btn')
+            .addEventListener('click', ()=>{
+                alert('조인버튼 클릭');
+                join();
+            });
         });
-
-        let login_btn = document.querySelector('#login_btn');
+        let login_btn = document.querySelector('#login-btn');
         login_btn.addEventListener('click',(e)=>{
             e.preventDefault();
             alert('로그인 버튼 클릭');
-            // count();
-            // 로그인 input 값 받기
-            // textbox안에 있는 값을 받아오는 것
-            id=document.getElementById('customerId').value;
-            pass=document.getElementById('password').value;
-
-
-            let data = {password:pass};
-            let json = JSON.stringify(pass);
-
-
+            id = document.getElementById('customerId').value;
+            pass = document.getElementById('password').value;
             let xhr = new XMLHttpRequest();
-            xhr.open('GET','customers/'+id,true);
-            xhr.setRequestHeader('Contect-type','application/json;charset=utf-8;')
+            xhr.open('GET', 'customers/'+id+'/'+pass, true);
             xhr.onload=()=>{
-                if(xhr.readyState===4 && xhr.status===200){
+                if(xhr.readyState=== 4 && xhr.status === 200){
                     if(xhr.responseText){
                         $wrapper.innerHTML = app.mypage();
                     }else{
-                        alert('로그인 실패');
                         $wrapper.innerHTML = app.login_form();
                     }
+                    
                 }
             };
-            xhr.send(json);
+            xhr.send();
         });
-    }
-
-    let count =() => {
-        //xhr 인스턴스 만들기
-        let xhr = new XMLHttpRequest();
-        method = 'GET';
-        //count url을 사용한다.
-        url='count';
-        //메소드와 url을 열고
-        xhr.open(method,url,true);
-        //함수 데이터가 변환할 준비가 됐다.
-        xhr.onreadystatechange=()=>{
-            // 준비상태가 정상, 정상이면
+    },
+    join : ()=>{
+        let xhr = new XMLHttpRequest()
+        xhr.open('POST','customers');
+        xhr.setRequestHeader('Content-type','application/json;charset=UTF-8');
+        xhr.send(JSON.stringify({'customerId':'hong', 'password':'1'}));
+    },
+    count : ()=>{
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'count', true);
+        xhr.onload=()=>{
             if(xhr.readyState===4 && xhr.status === 200){
-                //수행할 내용
                 alert('성공');
                 let wrapper = document.querySelector('#wrapper');
-                wrapper.innerHTML = ' 총 고객수 : <h1>'+xhr.responseText+'</h1>'
+                wrapper.innerHTML = '총 고객수 : <h1>'+xhr.responseText+'</h1>'
             }
         }
-        //준비상태 끝나면 보낸다.
         xhr.send();
     }
+}; 
 
-    //join form 만들기
-    let join_form = () => {
-        $wrapper.innerHTML = app.join_form();
 
-        //회원가입 확인 버튼 객체 만들기
-        let join_ok = document.querySelector('#join_ok')
-    
-        //확인 창 이벤트 걸기
-        join_ok.addEventListener('click',() => {
-    
-            //로그인 폼으로 가기
-            login_form();
-        });
-    }
 
-    
-    //클로저 : init의 값을 app으로 인식 시키기.
-    //json형태로 리턴값 주기
-    return {init : init};
-})();
-
-//app 영역 바깥에 만들어야 한다.
-  // 로그인 성공시
-app.mypage= () => {
-    // let wrapper = document.querySelector('#wrapper');
-    //  wrapper.innerHTML = 
-    return '<h1>마이페이지</h1>';
+app.mypage =()=>{
+    return '<h1>마이페이지</h1> ';
 };
 
-// 회원가입폼 
-app.join_form = () =>{
-    // wrapper 영역 전체에 보여질 내용 객체 만들기
-    return  '<form >'
-    +'  ID:<br>'
-    +'  <input type="text" name="id" >'
-    +'  <br>'
-    +'  PASSWORD:<br>'
-    +'  <input type="text" name="password" >'
-    +'  <br>'
-    +'  NAME:<br>'
-    +'  <input type="text" name="password" >'
-    +'  <br>'
-    +'  주민등록번호:<br>'
-    +'  <input type="text" name="password" >'
-    +'  <br>'
-    +'  휴대폰 번호:<br>'
-    +'  <input type="text" name="password" >'
-    +'  <br><br>'
-    +'  <input type="button"  value="확인" id="join_ok">'
-    +'  <input type="button"  value="취소" >'
-    +'</form> ';
-};
-
-//로그인 폼
 app.login_form=()=>{
-    // wrapper는 DOM 객체
-    return '<form >'
-    +'  ID:<br>'
-    +'  <input type="text" name="customerId" id="customerId">'
+    return '<form action="/action_page.php">'
+    +'  First name:<br>'
+    +'  <input type="text" id="customerId" name="customerId">'
     +'  <br>'
-    +'  PASSWORD:<br>'
-    +'  <input type="text" name="password" id="password">'
+    +'  Last name:<br>'
+    +'  <input type="text" id="password" name="password">'
     +'  <br><br>'
-    +'  <input type="button"  value="로그인" id=login_btn>'
-    +'  <input type="button"  value="회원가입" id=join_btn>'
+    +'  <input id="login-btn" type="button" value="LOGIN">'
+    +'  <input id="join-btn" type="button" value="JOIN">'
     +'</form> ';
 };
+app.join_form=()=>{
+    return 'PW<br>'
+    +'	<input type="password" name="pw"><br>'
+    +'	이름<br>'
+    +'	<input type="text" name="name"><br>'
+    +'	주민번호<br>'
+    +'	<input type="password" name="ssn"><br>'
+    +'	전화번호<br>'
+    +'	<input type="text" name="phone"><br><br>'
+    +'	<input id="confirm-btn" type="submit" value="확인">'
+    +'	<input id="cancel-btn" type="reset" value="취소">'
+    +'</form>';
+}
